4638bbb14bd196b3b9a31e7abd83449fd22935d4
Aclockworkobi <133705268+iabdullahks@users.noreply.github.com>
2025-03-17 22:46:59 +0500

    Add files via upload

---
+++ 99_bottles.py
@@ -0,0 +1,8 @@
+for bottles in range(99,0,-1):
+  print(f"{bottles} bottles of beer on the wall")
+  print(f"{bottles} bottles of beer")
+  print("Take one down,pass it around")
+if bottles-1>0:
+  print(f"{bottles} bottles of beer on the wall"/n)
+else:
+  print("No more bottles on the wall")
