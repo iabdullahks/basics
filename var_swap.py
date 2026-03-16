136bbfef474ba83311413f9ab863afe67e2cb9ea
Ahmad Abdullah Khan <133705268+iabdullahks@users.noreply.github.com>
2026-03-16 12:47:25 -0700

    Implement variable swapping in var_swap.py

---
+++ var_swap.py
@@ -0,0 +1,7 @@
+a = input("Enter the value of first variable(a) : ")
+b = input("Enter the value of second variable(b) : ")
+print(f"The values of the variables before swapping are a = {a} and b = {b}")
+temp = a
+a = b
+b = temp
+print(f"The values after swappint the variables are, a = {a} and b = {b}")
